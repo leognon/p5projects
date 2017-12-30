fed4d504517dfad3aa883415c07725146de9751a
Goel25 <leognon@gmail.com>
2017-12-30 12:15:34 -0500

    Started car class

--- carGA/sketch.js
+++ carGA/sketch.js
@@ -1,7 +1,40 @@
+let car;
+let lifetime = 250;
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(windowWidth, windowHeight);
+  car = new Car(width / 2, height / 2, 20, 50, 7, 0.4);
 }
 
 function draw() {
-  background(0);
+  background(220);
+
+  car.move();
+  car.show();
+}
+
+class Car {
+  constructor(x, y, w, h, maxSpeed, accRate, genes) {
+    this.pos = createVector(x, y);
+    this.acc = createVector();
+    this.vel = createVector(0, 0);
+    this.w = w;
+    this.h = h;
+    // this.genes = new DNA(maxSpeed, accRate, w, h);
+    this.genes = [];
+    for (let i = 0; i < lifetime; i++) {
+      this.genes.push(createVector(0.2, 0.4));
+    }
+  }
+
+  show() {
+    push();
+    translate(this.pos.x, this.pos.y);
+    rotate(this.vel.heading);
+    fill(50);
+    noStroke();
+    rect(-this.w / 2, -this.h / 2, this.w, this.h);
+
+    pop();
+  }
 }
