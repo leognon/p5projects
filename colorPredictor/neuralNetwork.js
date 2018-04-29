4546bc19fe65e7f9899d33c421e398c537dab294
Goel25 <leognon@gmail.com>
2018-04-29 16:11:23 -0400

    Added activation function (ReLu)

--- colorPredictor/neuralNetwork.js
+++ colorPredictor/neuralNetwork.js
@@ -15,6 +15,7 @@ class NeuralNetwork {
       console.log("The number of inputs must be the same as when the network was created!");
       return;
     }
+    inputs = inputs.map(x => x / 255); //Normalize inputs
 
     let hiddenValues = this.hiddenBiases.slice(0); //The array to hold the value of each hidden node based on the inputs, starting off with the biases so it doesn't have to be added later
     for (let j = 0; j < hiddenValues.length; j++) { //Compute each hidden node
@@ -22,6 +23,7 @@ class NeuralNetwork {
         hiddenValues[j] += (inputs[i] * this.inputHidden[i][j]);
       }
     }
+    hiddenValues = hiddenValues.map(x => activate(x)); //Activate each hidden node
 
     let outputs = this.outputBiases.slice(0); //The final outputs, starting out with the biases
     for (let j = 0; j < outputs.length; j++) { //Compute each hidden node
@@ -29,8 +31,13 @@ class NeuralNetwork {
         outputs[j] += (hiddenValues[i] * this.hiddenOutput[i][j]);
       }
     }
+    outputs = outputs.map(x => activate(x)); //Activate each output
     return outputs;
   }
+}
+
+function activate(n) { //The activation function
+  return max(0, n); //ReLu activation funciton
 }
 
 function create2dArray(rows, cols) {
@@ -38,7 +45,7 @@ function create2dArray(rows, cols) {
   for (let i = 0; i < rows; i++) {
     arr.push([]);
     for (let j = 0; j < cols; j++) {
-      arr[i].push(random(1)); //Randomize each value
+      arr[i].push(random(1) * 2 - 1); //Randomize each value
     }
   }
   return arr;
@@ -47,7 +54,7 @@ function create2dArray(rows, cols) {
 function randomArray(len) {
   let arr = [];
   for (let i = 0; i < len; i++) {
-    arr.push(random(1));
+    arr.push(random(1) * 2 - 1);
   }
   return arr;
 }
